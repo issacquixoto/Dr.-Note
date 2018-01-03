06433046477b0b2afbadc408b72552ab4d68f90a
Isaac <isac@smartshanghai.com>
2018-01-03 14:21:54 +0800

    配置smarty

---
+++ templates_c/1883f3af07bbaf431895d0cb1dfe84796cbd2815_0.file.index.tpl.php
@@ -0,0 +1,39 @@
+<?php
+/* Smarty version 3.1.31, created on 2018-01-03 07:19:52
+  from "/Applications/MAMP/htdocs/Dr.-Note/templates/templates/index.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.31',
+  'unifunc' => 'content_5a4c7608e236e3_65831512',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '1883f3af07bbaf431895d0cb1dfe84796cbd2815' => 
+    array (
+      0 => '/Applications/MAMP/htdocs/Dr.-Note/templates/templates/index.tpl',
+      1 => 1514951169,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5a4c7608e236e3_65831512 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    username:<?php echo $_smarty_tpl->tpl_vars['Name']->value;?>
+
+</body>
+</html><?php }
+}
--- www/index.php
+++ www/index.php
@@ -1,9 +1,14 @@
 <?php
-/*sample example */
 
-require_once('../vendor/autoload.php');
+require('../vendor/smarty/smarty/libs/Smarty.class.php');
 
 $smarty = new Smarty();
-$username = "Smarty";
+
+$smarty->template_dir = '../templates/';
+$smarty->compile_dir = '../templates_c/';
+$smarty->config_dir = '../configs/';
+$smarty->cache_dir = '../cache/';
+
+$username = "hello world";
 $smarty->assign("Name", $username);
-$smarty->display('index.tpl');
+$smarty->display('templates/index.tpl');
